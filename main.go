0a9cd1aa202b0c070d2d2ea8ba8a880f7b81d518
Jan Sorensen <sorensenjan@gmail.com>
2022-04-27 22:21:22 +0200

    Hardcode version in main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	_ "embed"
 	"flag"
 	"fmt"
 	"io"
@@ -12,32 +11,43 @@ import (
 	core "github.com/nesneros/xcat/xcatcore"
 )
 
-//go:embed .version
-var Version string
+var version string = "v0.1.4"
 
 func main() {
-	var showKindFlag bool
-	flag.BoolVar(&showKindFlag, "kind", false, "Print the detected kind")
-	flag.Usage = func() {
-		out := flag.CommandLine.Output()
-		fmt.Fprintf(out, "Version: %v, usage of %s:\n", Version, os.Args[0])
-		flag.PrintDefaults()
-		fmt.Fprintf(out, "\nPossible values for kind: %s\n", strings.Join(core.Kinds[:], ", "))
+	out := bufio.NewWriter(flag.CommandLine.Output())
+	defer out.Flush()
+
+	if err := run(os.Args, out, os.Stdin); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(3)
 	}
-	flag.Parse()
-	bufferedInput := bufio.NewReader(os.Stdin)
+}
+
+func run(args []string, out io.Writer, in io.Reader) error {
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	flags.Parse(os.Args)
+	flags.Usage = func() { usage(out, flags) }
+	showKind := flags.Bool("kind", false, "Print the detected kind")
+	flags.Parse(args[1:])
+
+	bufferedInput := bufio.NewReader(in)
 	size := bufferedInput.Size()
 	reader := core.NewReader(bufferedInput, size)
 	kind := reader.Kind()
-	if showKindFlag {
-		fmt.Printf("%v\n", kind)
-		return
+
+	if *showKind {
+		fmt.Fprintf(out, "%v\n", kind)
+		return nil
 	}
-	writer := bufio.NewWriter(os.Stdout)
-	written, err := io.Copy(writer, reader)
+	_, err := io.Copy(out, reader)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Written: %d bytes, error: %v", written, err)
-		os.Exit(1)
+		return err
 	}
-	writer.Flush()
+	return nil
+}
+
+func usage(out io.Writer, flags *flag.FlagSet) {
+	fmt.Fprintf(out, "Version: %v, usage of %s:\n", version, os.Args[0])
+	flags.PrintDefaults()
+	fmt.Fprintf(out, "\nPossible values for kind: %s\n", strings.Join(core.Kinds[:], ", "))
 }
