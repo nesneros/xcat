4a5a97030b82283389648e17cd26839f341514da
Jan Sorensen <sorensenjan@gmail.com>
2022-05-04 12:04:28 +0200

    Fix flush of stdout

--- main.go
+++ main.go
@@ -14,19 +14,18 @@ import (
 var version string = "v0.2.1"
 
 func main() {
-	out := bufio.NewWriter(flag.CommandLine.Output())
-	defer out.Flush()
-
-	if err := run(os.Args, out, os.Stdin); err != nil {
+	if err := run(os.Args, flag.CommandLine.Output(), os.Stdin); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(3)
 	}
 }
 
-func run(args []string, out io.Writer, in io.Reader) error {
+func run(args []string, w io.Writer, in io.Reader) error {
+	out := bufio.NewWriter(w)
+	defer out.Flush()
 	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
 	flags.Parse(os.Args)
-	flags.Usage = func() { usage(out, flags) }
+	flags.Usage = func() { usage(flags) }
 	showKind := flags.Bool("kind", false, "Print the detected kind")
 	flags.Parse(args[1:])
 
@@ -46,8 +45,8 @@ func run(args []string, out io.Writer, in io.Reader) error {
 	return nil
 }
 
-func usage(out io.Writer, flags *flag.FlagSet) {
-	fmt.Fprintf(out, "Version: %v, usage of %s:\n", version, os.Args[0])
+func usage(flags *flag.FlagSet) {
+	fmt.Fprintf(flags.Output(), "Version: %v, usage of %s:\n", version, os.Args[0])
 	flags.PrintDefaults()
-	fmt.Fprintf(out, "\nPossible values for kind: %s\n", strings.Join(xcat.Kinds[:], ", "))
+	fmt.Fprintf(flags.Output(), "\nPossible values for kind: %s\n", strings.Join(xcat.Kinds[:], ", "))
 }
